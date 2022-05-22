c19dd244725df97a543a703dd4e8489452113b3b
Kyle <dpkyle97@gmail.com>
2022-05-22 23:15:06 +0900

    5.22
    기본 세팅 및 오토레이아웃 작업.

--- starbucks/ViewController.swift
+++ starbucks/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonSet()
     }
 
 
+    func buttonSet() {
+        loginButton.layer.borderColor = .init(red: 0, green: 112/255, blue: 74/255, alpha: 1)
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.cornerRadius = loginButton.bounds.height / 2
+        
+        joinButton.layer.backgroundColor = .init(red: 0, green: 112/255, blue: 74/255, alpha: 1)
+        joinButton.layer.cornerRadius = joinButton.bounds.height / 2
+    }
 }
 
